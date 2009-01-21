01f28b8edbb6e0e4384bbccd5280f21bbf3772a4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-21 15:02:18 +0000

    use Pr(>|t|) for p values
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multcomp@254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.R
+++ pkg/R/print.R
@@ -36,8 +36,12 @@ print.summary.glht <- function(x, digits = max(3, getOption("digits") - 3),
     pq <- x$test
     mtests <- cbind(pq$coefficients, pq$sigma, pq$tstat, pq$pvalues)
     error <- attr(pq$pvalues, "error")
+    pname <- switch(x$alternativ,
+        "less" = paste("Pr(<", ifelse(x$df == 0, "z", "t"), ")", sep = ""),
+        "greater" = paste("Pr(>", ifelse(x$df == 0, "z", "t"), ")", sep = ""),
+        "two.sided" = paste("Pr(>|", ifelse(x$df == 0, "z", "t"), "|)", sep = ""))
     colnames(mtests) <- c("Estimate", "Std. Error",
-        ifelse(x$df == 0, "z value", "t value"), "p value")
+        ifelse(x$df == 0, "z value", "t value"), pname)
     type <- pq$type
 
     ### print p values according to simulation precision
